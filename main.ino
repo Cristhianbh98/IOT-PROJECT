7a021686bcd993b7e0f3875b2ca78561a9c62039
Cristhian Bacusoy <cristhianbacusoy@gmail.com>
2023-07-27 14:41:32 -0500

    finish main.ino

--- main.ino
+++ main.ino
@@ -1,9 +1,16 @@
-// Importar Librerias
 #include <DHT.h>
-#include <Wire.h>
-#include "ESP8266WiFi.h"
+#include <ESP8266WiFi.h>
 #include <PubSubClient.h>
+#include <Wire.h>
 #include <LiquidCrystal_I2C.h>
+
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+DHT dht(A0, DHT11);
+int rele = 2;
+int h;
+int pause = 800;
+unsigned long start_time;
+String payload;
 
 //-------------------VARIABLES GLOBALES--------------------------
 // Configuración de Red
@@ -17,21 +24,11 @@ PubSubClient client(espClient);
 unsigned long last_send;
 int status = WL_IDLE_STATUS;
 
-// Configuración de sensor
-LiquidCrystal_I2C lcd(0x27, 16, 2);
-DHT dht(A0, DHT11);
-float h;
-
-// Definición de la variable para publicar el mensaje
-String payload;
-int rele = 2;
-int pause = 800;
-
 // Esta función reconecta su ESP8266 al Broker (MQTT Server)
 void reconnect() {
-  while ( !client.connected() ) {
-    if ( client.connect("A") ) {
-      Serial.println("ESP8266 Conectado a MQTT");
+ while ( !client.connected() ) {
+   if (client.connect("CLIENT_SP")) {
+     Serial.println("ESP8266 Conectado MQTT");
     } else {
       Serial.print("Fallo, rc=");
       Serial.print(client.state());
@@ -39,6 +36,58 @@ void reconnect() {
       delay(1000);
     }
   }
+}
+
+// Esta función lee el sensor de humedad
+void get_hume() {
+  h = map(analogRead(A0), 0, 1023, 100, 0);
+
+  if ( isnan(h) ) {
+    lcd.setCursor(0, 0);
+    lcd.print("Error sensor");
+    delay(1500);
+    return;
+  }
+
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Humedad = ");
+  lcd.print(h);
+  lcd.print("%");
+  delay(3000);
+
+  if( h < 30) {
+    lcd.setCursor(0, 1);
+    lcd.print("Iniciando Riego");
+    delay(3000);
+
+    digitalWrite(rele, HIGH);
+    lcd.clear();
+    lcd.print("Regando suelo...");
+    delay(3000);
+    digitalWrite(rele, LOW);
+    
+    lcd.clear();
+    lcd.print("Apagando riego");
+
+    delay(3000);    
+  }
+  else {
+    lcd.setCursor(0, 1);
+    lcd.print("Todo en orden :3");
+    delay(3000);
+  }
+
+  lcd.clear();
+
+  lcd.setCursor(0, 0);
+  lcd.print("Tomando nivel");
+  lcd.setCursor(2, 1);
+  lcd.print("de humedad");
+  
+  delay(3000);
+
+  lcd.clear();
 }
 
 // Configuración de Red
@@ -59,82 +108,48 @@ void setup_wifi() {
   Serial.println(WiFi.localIP());
 }
 
-// Leer temperatura y humedad
-void get_data(){
-  h = map(analogRead(A0), 0, 1023, 100, 0);
-
-  if ( isnan(h) ) {
-    Serial.println("Fallo al leer el sensor DHT!");
-    delay(1500);
-    return;
-  }
-  
-  Serial.print("Nivel de Humedad = ");
-  Serial.print(h);
-  Serial.println("%");
-  delay(5000);
-}
-
 // Configuración formato Json
 void get_payload(){
-  String humedad = String(h);
-  String temperatura = String(20);
-  payload = "{";
-  payload += "\"t\":"; payload += temperatura; payload += ",";
-  payload += "\"h\":"; payload += humedad;
-  payload += "}";
+ String humedad = String(h);
+ String temperatura = String(20);
+ payload = "{";
+ payload += "\"t\":"; payload += temperatura; payload += ",";
+ payload += "\"h\":"; payload += humedad;
+ payload += "}";
 }
 
 // Enviar datos al servidor
 void send_payload() {
-  char attributes[100];
-  payload.toCharArray( attributes, 100 );
-
-  if ( client.publish( "IOT/TEMHUM", attributes ) ) {
-    Serial.println( "Mensaje enviado correctamente" );
-  } else {
-    Serial.println( "Fallo al enviar el mensaje" );
-  }
-
-  Serial.println( attributes );
+ char attributes[100];
+ payload.toCharArray( attributes, 100 );
+ client.publish( "IOT/TEMHUM", attributes );
+ Serial.println( attributes );
 }
 
-// Configuración de la función (setup)
 void setup() {
-  Serial.begin(9600);
-  setup_wifi();
-  client.setServer(mqtt_server, 1883);
-  pinMode(rele, OUTPUT);
+  //Wire.begin();
+  dht.begin();
   lcd.init();
   lcd.clear();
   lcd.backlight();
-  dht.begin();
-  last_send = 0;
+  Serial.begin(9600);
+  pinMode(rele, OUTPUT);
+  start_time = millis();
+  setup_wifi();
+  client.setServer(mqtt_server, 1883);
   delay(3000);
 }
+
 
 void loop() {
   if ( !client.connected() ) {
     reconnect();
   }
 
-  if ( millis() - last_send > 5000 ) {
-    get_data();
+  if ( millis() - start_time > 5000 ) {
+    get_hume();
     get_payload();
     send_payload();
-    Serial.println(payload);
-    last_send = millis();
+    start_time = millis();
   }
-
-  client.loop();
-
-  /* if( h < 30) {
-    Serial.println("Nivel de humedad bajo, Iniciando sistema de riego");
-
-    digitalWrite(rele, LOW);
-    delay(3000);
-    digitalWrite(rele, HIGH);
-    Serial.println("Sistema de riego desactivado");   
-    delay(3000);
-  } */
 }
